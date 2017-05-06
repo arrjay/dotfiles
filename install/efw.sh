70a24a8d6c275e737f69f70fcb543ac5affd3dcc
RJ Bergeron <rj@arrjay.net>
2017-05-06 08:04:50 +0000

    start updating vms to deal with the vmm interface

--- install/efw.sh
+++ install/efw.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-virt-install -nefw -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/efw.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=uplink,model=virtio -w bridge=transit,model=virtio --noautoconsole
+virt-install -nefw -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/efw.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=uplink,model=virtio -w bridge=transit,model=virtio -w bridge=transit,model=virtio --noautoconsole
