26a2f50e132c656a26cba942c1d58d44e66497a9
RJ Bergeron <rj@arrjay.net>
2017-05-06 08:20:43 +0000

    initial ifw work for vmm

--- install/ifw.sh
+++ install/ifw.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-virt-install -nifw -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/ifw.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=transit,model=virtio -w bridge=dmz,model=virtio -w bridge=virthost,model=virtio -w bridge=netmgmt,model=virtio -w bridge=standard,model=virtio -w bridge=restricted,model=virtio --noautoconsole
+virt-install -nifw -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/ifw.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=transit,model=virtio -w bridge=vmm,model=virtio -w bridge=dmz,model=virtio -w bridge=virthost,model=virtio -w bridge=netmgmt,model=virtio -w bridge=standard,model=virtio -w bridge=restricted,model=virtio --noautoconsole
