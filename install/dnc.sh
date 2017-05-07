5a2ff5600413759660cd5ca6c24072cd780d1d4c
RJ Bergeron <rj@arrjay.net>
2017-05-07 19:07:53 +0000

    vmm for dnc

--- install/dnc.sh
+++ install/dnc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-virt-install -ndnc -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/dnc.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=dmz,model=virtio --noautoconsole
+virt-install -ndnc -r128 --os-variant=openbsd5.8 --disk /var/lib/libvirt/images/dnc.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=dmz,model=virtio -w bridge=vmm,model=virtio --noautoconsole
