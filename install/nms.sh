61a0e4a558443a4d6863611dd64f19d42091a6ed
RJ Bergeron <rj@arrjay.net>
2017-05-07 21:10:21 +0000

    vmm network for nms

--- install/nms.sh
+++ install/nms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-virt-install -nnms -r512 --os-variant=centos7.0 --disk /var/lib/libvirt/images/nms.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=dmz,model=virtio --noautoconsole
+virt-install -nnms -r512 --os-variant=centos7.0 --disk /var/lib/libvirt/images/nms.qcow2,device=disk,bus=virtio --vcpus=1 --import -w bridge=dmz,model=virtio -w bridge=vmm,model=virtio --noautoconsole
