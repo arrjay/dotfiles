9bf8f4e6c5c114903a397cbafe16fd1137ab4d8f
RJ Bergeron <rj@arrjay.net>
2017-10-02 20:15:08 +0000

    move scripts around to be usable in the instance

--- install-netmgmt.sh
+++
@@ -1,1 +0,0 @@
-virt-install --location http://192.168.128.129/bootstrap/centos7/ --name netmgmt --memory 1536 --disk size=12 --graphics none --extra-args="console=ttyS0,115200n8" --network bridge=vmm
