485bf2ac2b6855c17123a2e516427a9d2baca073
RJ Bergeron <rj@arrjay.net>
2018-10-08 03:52:55 +0000

    move editor/pager stuff to extensions

---
+++ bash.d/extensions/editor_pager.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# (re)-configure EDITOR / PAGER dependent on programs installed.
+
+# if I have vim, alias vi to it
+chkcmd vim && vi () { command vim "${@}" ; }
+
+# check if we already have EDITOR defined to vim or...editplus ;)
+case "${EDITOR}" in
+  *vim*|*editplus*) : ;;
+  *)
+    # well...let's try vi first.
+    chkcmd vi  && export EDITOR='vi'
+    chkcmd vim && export EDITOR='vim'
+    [ "${___x11_environment}" == 'yes' ] && {
+      chkcmd gvim && export EDITOR='gvim -f'
+    }
+  ;;
+esac
+
+# configure a pager, more or less
+chkcmd more && export PAGER='more'
+chkcmd less && export PAGER='less'
