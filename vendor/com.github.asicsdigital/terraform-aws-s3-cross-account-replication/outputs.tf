2a96779c8349da37482d3de200730e71f8a15cf7
RJ Bergeron <rj@arrjay.net>
2019-01-14 07:25:05 +0000

    remove source write user

--- vendor/com.github.asicsdigital/terraform-aws-s3-cross-account-replication/outputs.tf
+++ vendor/com.github.asicsdigital/terraform-aws-s3-cross-account-replication/outputs.tf
@@ -3,14 +3,3 @@
 output "dest_bucket_policy_json" {
   value = "${var.create_dest_bucket == "true" ? "not needed" : data.aws_iam_policy_document.dest_bucket_policy.json}"
 }
-
-# Source write IAM user
-
-output "source_write_iam_user_access_key_id" {
-  value = "${aws_iam_access_key.source_write.id}"
-}
-
-output "source_write_iam_user_secret_access_key" {
-  value     = "${aws_iam_access_key.source_write.secret}"
-  sensitive = true
-}
