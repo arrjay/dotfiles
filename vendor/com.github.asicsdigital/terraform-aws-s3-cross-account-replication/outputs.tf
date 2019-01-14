192c0716d0711f561b4ba0f583c13d1f9d71ed01
RJ Bergeron <rj@arrjay.net>
2019-01-14 07:52:55 +0000

    add an output of the created bucket ARN instead

--- vendor/com.github.asicsdigital/terraform-aws-s3-cross-account-replication/outputs.tf
+++ vendor/com.github.asicsdigital/terraform-aws-s3-cross-account-replication/outputs.tf
@@ -3,3 +3,7 @@
 output "dest_bucket_policy_json" {
   value = "${var.create_dest_bucket == "true" ? "not needed" : data.aws_iam_policy_document.dest_bucket_policy.json}"
 }
+
+output "source_bucket_arn" {
+  value = "${aws_s3_bucket.source.arn}"
+}
