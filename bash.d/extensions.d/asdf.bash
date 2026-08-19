e4e85f5ccf0b5c9ca51a09f37ae3a3b0d5eab39c
RJ Bergeron <rj@arrjay.net>
2026-08-19 01:11:39 +0000

    update asdf with similar load function to homebrew, fix exec on homebrew loader

--- bash.d/extensions.d/asdf.bash
+++ bash.d/extensions.d/asdf.bash
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-[[ -x "${HOME}/.asdf/asdf.sh" ]] && {
-  source "${HOME}/.asdf/asdf.sh"
+## asdf - prefer our version over the system one, but
+___hook_asdf () {
+  local asdf
+  for asdf in "${HOME}/.asdf/asdf.sh" /opt/homebrew/opt/asdf/libexec/asdf.sh ; do
+    [[ -e "${asdf}" ]] && . "${asdf}" && return 0
+  done
+  return 0
 }
+
+___hook_asdf
+unset -f ___hook_asdf
